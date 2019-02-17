98ba1a1d7fda6f90994758a8c0df8970308aefce
공채원 <chaewonkong@gmail.com>
2019-02-17 23:18:34 +0900

    Add setupProxy to client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello there~");
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
