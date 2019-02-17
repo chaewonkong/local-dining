19a812c8c2e026e6fa4c78908f30e80ecafdaa37
공채원 <chaewonkong@gmail.com>
2019-02-17 23:06:22 +0900

    Add body-parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello there~");
