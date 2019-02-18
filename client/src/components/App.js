9cc0c046ab0e46db36ff73cc898195f9fcd26195
공채원 <chaewonkong@gmail.com>
2019-02-18 16:35:32 +0900

    Add material-ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Header from "./Header";
+import { Layout } from "antd";
 import Landing from "./Landing";
 import DetailView from "./DetailView";
+import Header from "./Header";
+import "../App.css";
+
+const { Footer, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <div>
-            <Header />
-          </div>
-          <Route exact path="/" component={Landing} />
-          <Route path="/places" component={DetailView} />
+          <Header
+            style={{
+              position: "fixed",
+              zIndex: 1,
+              width: "100%",
+              background: "#fff",
+              height: 64
+            }}
+          >
+            This is Header
+          </Header>
+          <Content style={{ paddingTop: "64px" }}>
+            <Route exact path="/" component={Landing} />
+            <Route path="/places" component={DetailView} />
+          </Content>
+          <Footer style={{ position: "absolute", bottom: 0 }}>
+            This is Footer
+          </Footer>
         </div>
       </BrowserRouter>
     );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Input } from "antd";
 
+const Search = Input.Search;
 const Header = () => {
   return (
-    <div>
-      <h1>This is Header</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <h1>우리동네 착한밥상</h1>
+      <div>
+        <Search
+          placeholder="input search text"
+          enterButton
+          size="large"
+          onSearch={value => console.log(value)}
+        />
+      </div>
     </div>
   );
 };
