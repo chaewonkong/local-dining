daa4af49f02574d35454ad52aad3a339a51a8aa7
공채원 <chaewonkong@gmail.com>
2019-03-18 23:12:01 +0900

    Add PointSchema

--- server/models/Place.js
+++ server/models/Place.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-const PointSchema = new Schema({
-  type: { type: String, default: "Point" },
-  coordinates: { type: [Number], index: "2dsphere" }
-});
+const PointSchema = require("./Point");
 
 const MenuSchema = new Schema({
   name: { type: String },
